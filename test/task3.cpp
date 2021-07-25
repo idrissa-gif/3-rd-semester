ef86f9c16a66e99d2d30da3f9cf3b3352fc1868c
190041242_Idrissa_CSE_2B <idrissa@iut-dhaka.edu>
2021-07-25 23:07:10 +0600

    first commit

---
+++ test/task3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+vector<pair<int, int>> pairSum(int arr[], int n, int sum)
+{
+    vector<pair<int, int>> a;
+
+    return a;
+}
+int main()
+{
+    int n, sum;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[n];
+    cin >> sum;
+    vector<pair<int, int>> a = pairSum(arr, n, sum);
+}
